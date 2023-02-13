79c2ba2090997ef5f534d1659f8786aea019c793
Shreshta-Chidrapu <chidrapu.shreshta@rtzen.ai>
2023-02-13 17:33:47 +0530

    updated studentController

--- src/main/java/com/example/example/controller/StudentController.java
+++ src/main/java/com/example/example/controller/StudentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class StudentController {
   @Autowired
     private StudentService studentService;
-
+//create a record
     @PostMapping("/save")
     @ResponseBody
     public  Student saveStudent(@RequestBody Student student){
