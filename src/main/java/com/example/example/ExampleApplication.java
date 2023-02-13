aafea2ea0556a643a8b345fd6380266cc0c0e1d6
Shreshta-Chidrapu <chidrapu.shreshta@rtzen.ai>
2023-02-13 17:31:43 +0530

    updated gitignore

--- src/main/java/com/example/example/ExampleApplication.java
+++ src/main/java/com/example/example/ExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExampleApplication {
-
+ //Sample change to check github
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
